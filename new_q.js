d412e2325ffc7fdf07ca6d053b77ae3e11b60e6c
11mones <mones_leader@hotmail.com>
2023-03-09 17:22:34 +0000

    modify somethings in js

--- new_q.js
+++ new_q.js
@@ -1,26 +1,29 @@
+'use strict'
 var array =[];
 function threeQuestions (array){
     
         array[0] = prompt("Do you like watching football games ? (Y/N)");
-        if(array[0].length == 0){
-            array[0] = "invalid";
-        }
+     
 
         array[1] = prompt("Do you like chess ? (Y/N)");
-        if(array[1].length == 0){
-            array[1] = "invalid";
-        }
+     
         array[2] = prompt("Have you ever tried before to manage your time ? (Y/N)");
-        if(array[2] .length == 0){
-            array[2] = "invalid";
-        }
+      
 }
+function fillArray(array){
+    for(let i =0 ;i<=2;i++){
+        if(array[i] .length == 0){
+            array[i] = "invalid";
+        }
+    }
+}
+
 function printArr(array){
 for(let i =0;i<=2;i++){
     console.log(array[i]);
     console.log("\n");
 }
-
 }
 threeQuestions(array);
+fillArray(array);
 printArr(array);
